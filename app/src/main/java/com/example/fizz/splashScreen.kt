ba6c16ae4d60b75e929437154839050e35a5e88f
Vivek Chaurasiya <cvivek9933@gmail.com>
2023-06-12 22:54:54 +0530

    first commit

---
+++ app/src/main/java/com/example/fizz/splashScreen.kt
@@ -0,0 +1,27 @@
+package com.example.fizz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class splashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+   Handler(Looper.getMainLooper()).postDelayed({
+
+       val i = Intent(this,MainActivity::class.java)
+       startActivity(i)
+finish()
+
+
+
+
+   },3000)
+
+
+    }
+}
